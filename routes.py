d728439b0bab626c43ae91ffb75998f4eb8e5c37
Maribel <maribel-obando.j@utp.edu.co>
2022-02-04 15:07:23 -0500

    routes file again

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5432/misaluddb"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://itbhznlmjvxpho:207139a015697300f6a516a2440fc6a6c1aed80d2857235f514be7075e374c3d@ec2-54-157-15-228.compute-1.amazonaws.com:5432/deee8u79387j9s"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://itbhznlmjvxpho:207139a015697300f6a516a2440fc6a6c1aed80d2857235f514be7075e374c3d@ec2-54-157-15-228.compute-1.amazonaws.com:5432/deee8u79387j9s'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'some-secret-key'
 
@@ -22,7 +22,7 @@ def front_page():
     return "this works"
 
 @app.route('/welcome')
-def front_page():
+def welcome_page():
     return render_template("home.html")
 
 @app.route('/useraccount', methods=['POST'])
